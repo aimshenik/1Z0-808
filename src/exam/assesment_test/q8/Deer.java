7a3ce06236ac586e007f95e239df0bf4a7fb375a
Imshenik <andrey.imshenik@thomsonreuters.com>
2019-06-11 15:57:45 +0300

    commit

--- src/exam/assesment_test/q8/Deer.java
+++ src/exam/assesment_test/q8/Deer.java
@@ -52,3 +52,14 @@ class Reindeer extends Deer {
         return true;
     }
 }
+/*
+A. The code compiles and runs without issue, so options G and H are incorrect. First,
+the Reindeer object is instantiated using the constructor that takes an int value. Since
+there is no explicit call to the parent constructor, the default no-argument super()
+is inserted as the first line of the constructor. The output is then Deer, followed by
+Reindeer in the child constructor, so only options A and B can be correct. Next,
+the method hasHorns() looks like an overridden method, but it is actually a hidden
+method since it is declared private in the parent class. Because the hidden method is
+referenced in the parent class, the parent version is used, so the code outputs false,
+and option A is the correct answer.
+ * */
