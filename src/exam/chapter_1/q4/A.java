ca321da5acb45a4fa409f902110bf1617f2e2c46
Imshenik <andrey.imshenik@thomsonreuters.com>
2019-06-21 17:33:35 +0300

    bla bla

--- src/exam/chapter_1/q4/A.java
+++ src/exam/chapter_1/q4/A.java
@@ -22,3 +22,15 @@ E. None of the above changes will make the code compile.
 
 public class A {
 }
+
+/*
+4. A, B. Adding the variable at line 2 makes result an instance variable. Since instance
+variables are in scope for the entire life of the object, option A is correct. Option B is
+correct because adding the variable at line 4 makes result a local variable with a scope
+of the whole method. Adding the variable at line 6 makes result a local variable with
+a scope of lines 6–7. Since it is out of scope on line 8, the println does not compile and
+option C is incorrect. Adding the variable at line 9 makes result a local variable with
+a scope of lines 9 and 10. Since line 8 is before the declaration, it does not compile and
+option D is incorrect. Finally, option E is incorrect because the code can be made to
+compile. 
+ * */
